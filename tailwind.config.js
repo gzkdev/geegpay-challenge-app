5da08eea80b42ea8892934a0e3ad0d7469320f19
Ezihe Godswill <ezihegodswill01@gmail.com>
2024-01-26 07:11:34 +0100

    update tailwind config  and global styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,25 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "alert-error": "#ED544E",
+        "alert-success": "#66C87B",
+        "alert-warning": "#F4C700",
+        "gray-900": "#171717",
+        "gray-50": "#FAFAFA",
+        "gray-100": "#F5F5F5",
+        "gray-200": "#E5E5E5",
+        "gray-300": "#D4D4D4",
+        "gray-400": "#A3A3A3",
+        "gray-500": "#737373",
+        "gray-600": "#525252",
+        "gray-700": "#404040",
+        "gray-800": "#262626",
+        "main-green": "#34CAA5",
+        "main-text": "#0D062D",
+      },
+    },
   },
   plugins: [],
 };
